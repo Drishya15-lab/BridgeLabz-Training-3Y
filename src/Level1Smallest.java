c93a97557a25685e7303477a4a628e9c9a49864d
Drishya <drishya.singh_cs23@gla.ac.in>
2025-08-20 10:53:35 +0530

    Initial push: Java Control flows programs

---
+++ src/Level1Smallest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Level1Smallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        boolean isSmallest = (num1 < num2) && (num1 < num3);
+        System.out.println("Is the first number the smallest? " + isSmallest);
+    }
+}
